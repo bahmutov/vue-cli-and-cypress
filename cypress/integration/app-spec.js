25da5b172a30054e5a611242200a4e3b1f6c108f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-27 15:37:55 -0500

    test if image has loaded

--- cypress/integration/app-spec.js
+++ cypress/integration/app-spec.js
@@ -8,4 +8,23 @@ describe('App', () => {
   it('welcomes you', () => {
     cy.contains('Welcome to Your Vue.js App')
   })
+
+  it('loads logo', () => {
+    cy.wait(100)
+    cy.log('Logo image naturalWidth')
+    cy
+      .get('img')
+      .then($img => {
+        return $img[0].naturalWidth
+      })
+      .should('be.gt', 0)
+  })
+
+  it('loads logo using its', () => {
+    cy.log('Logo image naturalWidth')
+    cy
+      .get('img')
+      .its('0.naturalWidth')
+      .should('be.gt', 0)
+  })
 })
