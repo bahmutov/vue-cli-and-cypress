e6d76afc4747bcb5b97de3088e3c825fdd0803ee
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-27 14:17:12 -0500

    loadig static logo from dist folder

--- cypress/integration/app-spec.js
+++ cypress/integration/app-spec.js
@@ -2,7 +2,8 @@ const mountVue = require('cypress-vue-unit-test')
 import App from '../../src/App.vue'
 
 describe('App', () => {
-  beforeEach(mountVue(App))
+  const options = { vue: 'https://unpkg.com/vue', base: '/' }
+  beforeEach(mountVue(App, options))
 
   it('welcomes you', () => {
     cy.contains('Welcome to Your Vue.js App')
